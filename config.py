bc553f0e42e788fc819cea988a9ea14600ee7684
Vipchor <randhirkumar4873@gmail.com>
2024-12-01 08:35:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Vipchor/BADNAMXMUSIC",
+    "https://github.com/Vipchor/KANHA_MIX_MUSIC_bot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
